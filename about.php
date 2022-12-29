da5580848bcc08e54cd77e3f0b1e9ed911eccc32
Sergiololpro <homerjsimp@yandex.ru>
2022-12-29 13:48:37 +0300

    About scheme

--- about.php
+++ about.php
@@ -178,6 +178,53 @@
         Разнообразный и богатый опыт говорит нам, что внедрение современных методик, в своём классическом представлении, допускает внедрение новых предложений. Принимая во внимание показатели успешности, новая модель организационной деятельности предоставляет широкие возможности для кластеризации усилий. Имеется спорная точка зрения, гласящая примерно следующее: некоторые особенности внутренней политики лишь добавляют фракционных разногласий и заблокированы в рамках своих собственных рациональных ограничений. Наше дело не так однозначно, как может показаться: существующая теория требует анализа новых предложений.
     </div>
   </div>
+
+  <div class="b_block">
+    <div class="wrapper">
+      <div class="scheme_top__wp">
+          <div class="scheme_nav">
+              <div data-id="0" class="scheme_nav__el active">
+                  <div class="scheme_nav__circle">G</div>
+                  <div class="scheme_nav__text">Gallery 11</div>
+              </div>
+              <div data-id="1" class="scheme_nav__el">
+                  <div class="scheme_nav__circle">1</div>
+                  <div class="scheme_nav__text">Здание 1</div>
+              </div>
+              <div data-id="2" class="scheme_nav__el">
+                  <div class="scheme_nav__circle">2</div>
+                  <div class="scheme_nav__text">Здание 2</div>
+              </div>
+              <div data-id="3" class="scheme_nav__el">
+                  <div class="scheme_nav__circle">3</div>
+                  <div class="scheme_nav__text">Здание 3</div>
+              </div>
+              <div data-id="4" class="scheme_nav__el">
+                  <div class="scheme_nav__circle">4</div>
+                  <div class="scheme_nav__text">Здание 4</div>
+              </div>
+              <div data-id="5" class="scheme_nav__el">
+                  <div class="scheme_nav__circle">5</div>
+                  <div class="scheme_nav__text">Здание 5</div>
+              </div>
+              <div data-id="6" class="scheme_nav__el">
+                  <div class="scheme_nav__circle">6</div>
+                  <div class="scheme_nav__text">Здание 7</div>
+              </div>
+          </div>
+          <div class="scheme_top__map">
+              <img class="scheme_top__img" src="img/scheme_top__img.png" alt="">
+              <img data-id="0" class="scheme_top__sector scheme_top__sector-0 active" src="img/scheme_top__sector-0.png" alt="">
+              <img data-id="1" class="scheme_top__sector scheme_top__sector-1" src="img/scheme_top__sector-1.png" alt="">
+              <img data-id="2" class="scheme_top__sector scheme_top__sector-2" src="img/scheme_top__sector-2.png" alt="">
+              <img data-id="3" class="scheme_top__sector scheme_top__sector-3" src="img/scheme_top__sector-3.png" alt="">
+              <img data-id="4" class="scheme_top__sector scheme_top__sector-4" src="img/scheme_top__sector-4.png" alt="">
+              <img data-id="5" class="scheme_top__sector scheme_top__sector-5" src="img/scheme_top__sector-5.png" alt="">
+              <img data-id="6" class="scheme_top__sector scheme_top__sector-6" src="img/scheme_top__sector-6.png" alt="">
+          </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php include 'includes/footer.php'; ?>
